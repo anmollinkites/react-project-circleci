b14957341ba1b3f997815fb6b40931478e6ce36a
anmollinkites <anmolmathur@linkites.com>
2021-06-28 17:58:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-project-circleci
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-project-circleci)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+import Hello from './Components/Hello';
+export default function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
